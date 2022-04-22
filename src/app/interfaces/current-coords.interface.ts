a4c98f5a03adfdf21aeebb3b8efa0d537ceb03e0
FCastilloSorani <fercastillo.s.1997@gmail.com>
2022-04-22 02:11:03 -0300

    add environment to gitignore

--- src/app/interfaces/current-coords.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface CurrentCoords {
-  longitude: number | null;
-  latitude: number | null;
-}
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  API_KEY: 'your_api_key',
-
-  defaultLongitude: 0,
-  defaultLatitude: 0,
-};
